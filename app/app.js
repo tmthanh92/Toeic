e0acc22d499863374e230cc7afad24bc9850ae02
Thanh Nguyen <nhatthanh510@gmail.com>
2016-05-03 13:20:45 +0700

    Add social login

--- app/app.js
+++ app/app.js
@@ -5,6 +5,38 @@
     .module('app', ['ngRoute', 'ngCookies'])
     .config(config);
 
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '270946653240452',
+            cookie     : true,  // enable cookies to allow the server to access
+                                // the session
+            xfbml      : true,  // parse social plugins on this page
+            version    : 'v2.6' // use graph api version 2.6
+        });
+
+        // Now that we've initialized the JavaScript SDK, we call
+        // FB.getLoginStatus().  This function gets the state of the
+        // person visiting this page and can return one of three states to
+        // the callback you provide.  They can be:
+        //
+        // 1. Logged into your app ('connected')
+        // 2. Logged into Facebook, but not your app ('not_authorized')
+        // 3. Not logged into Facebook and can't tell if they are logged into
+        //    your app or not.
+        //
+        // These three cases are handled in the callback function.
+    };
+
+    // Load the SDK asynchronously
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+
   config.$inject = ['$routeProvider', '$locationProvider'];
 
   function config($routeProvider, $locationProvider) {
@@ -16,9 +48,10 @@
       })
       .when('/login', {
         controller: 'LoginController',
+        templateUrl: 'login/login.html',
         controllerAs: 'vm'
       })
       .when('/', { redirectTo: '/home' });
-
   }
+
 })();
--- app/login/loginController.js
+++ app/login/loginController.js
@@ -38,9 +38,39 @@
       $location.path('/login');
     }
 
+    function fbLogin() {
+      FB.getLoginStatus(function(response) {
+        if (response.status === 'connected') {
+          // the user is logged in and has authenticated your
+          // app, and response.authResponse supplies
+          // the user's ID, a valid access token, a signed
+          // request, and the time the access token
+          // and signed request each expire
+          var uid = response.authResponse.userID;
+          var accessToken = response.authResponse.accessToken;
+        } else if (response.status === 'not_authorized') {
+          // the user is logged in to Facebook,
+          // but has not authenticated your app
+        } else {
+          // the user isn't logged in to Facebook.
+          FB.login(function(response) {
+            if (response.authResponse) {
+              console.log('Welcome!  Fetching your information.... ');
+              FB.api('/me', function(response) {
+                console.log('Good to see you, ' + response.name + '.');
+              });
+            } else {
+              console.log('User cancelled login or did not fully authorize.');
+            }
+          });
+        }
+      });
+    }
+
     vm.login = login;
     vm.register = register;
     vm.logOut = logOut;
+    vm.fbLogin = fbLogin;
   }
 
   LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService', 'UserService'];
